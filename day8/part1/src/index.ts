36f447fad345c5f1ac486994dbc79e837c4360f1
Kiana McNellis <kianam@google.com>
2020-12-07 21:48:19 -0800

    Day 8

---
+++ day8/part1/src/index.ts
@@ -0,0 +1,57 @@
+var fs = require('fs');
+
+interface Instruction {
+  op: string;
+  arg: number;
+}
+const opRegex = new RegExp(/^(\w{3}) \+?(-?\d*)$/m)
+
+fs.readFile('input.txt', 'utf8', function(err: any, contents: string) {
+  if (err != null) {
+    console.error("Error", err);
+    return;
+  }
+  const instructions: Array<Instruction> = contents.trim().split("\n").map(
+    (x: string): Instruction => {
+      let res = x.match(opRegex)
+      if (res == null) {
+        console.log("error parsing", x)
+        return null
+      }
+      return {
+        op: res[1],
+        arg: parseInt(res[2])
+      }
+    }
+  );
+  console.log(instructions)
+  const result = runComputer(instructions)
+  console.log("result", result)
+});
+
+const runComputer = (code: Array<Instruction>): number => {
+  let visited: Set<number> = new Set()
+  let acc = 0
+  let index = 0
+
+  while (true) {
+    if (visited.has(index)) {
+      break;
+    }
+    visited.add(index)
+    let inst = code[index]
+    switch (inst.op) {
+      case "nop":
+        index += 1;
+        break;
+      case "acc":
+        index += 1;
+        acc += inst.arg
+        break;
+      case "jmp":
+        index += inst.arg
+        break;
+    }
+  }
+  return acc
+}
---
+++ day8/part2/src/index.ts
@@ -0,0 +1,70 @@
+var fs = require('fs');
+
+interface Instruction {
+  op: string;
+  arg: number;
+}
+const opRegex = new RegExp(/^(\w{3}) \+?(-?\d*)$/m)
+
+fs.readFile('input.txt', 'utf8', function(err: any, contents: string) {
+  if (err != null) {
+    console.error("Error", err);
+    return;
+  }
+  const instructions: Array<Instruction> = contents.trim().split("\n").map(
+    (x: string): Instruction => {
+      let res = x.match(opRegex)
+      if (res == null) {
+        console.log("error parsing", x)
+        return null
+      }
+      return {
+        op: res[1],
+        arg: parseInt(res[2])
+      }
+    }
+  );
+  for (let i = 0; i < instructions.length; i++) {
+    if (instructions[i].op != "acc") {
+      let code = Array.from(instructions)
+      code[i] = {
+        op: (code[i].op == "nop" ? "jpm" : "nop"),
+        arg: code[i].arg
+      }
+      try {
+        const result = runComputer(code)
+        console.log("result found on", i, ":", result)
+      }
+      catch (err) {
+        // console.log("result not", i, err)
+      }
+    }
+  }
+});
+
+const runComputer = (code: Array<Instruction>): number => {
+  let visited: Set<number> = new Set()
+  let acc = 0
+  let index = 0
+
+  while (index != code.length) {
+    if (visited.has(index)) {
+      throw new Error("Infinite loop at " + index)
+    }
+    visited.add(index)
+    let inst = code[index]
+    switch (inst.op) {
+      case "nop":
+        index += 1;
+        break;
+      case "acc":
+        index += 1;
+        acc += inst.arg
+        break;
+      case "jmp":
+        index += inst.arg
+        break;
+    }
+  }
+  return acc
+}
